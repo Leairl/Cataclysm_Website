60ffe27a047d0b096d4e826ca848c85718c84183
Lea Menefee <awaken2001@gmail.com>
2024-11-03 02:18:48 -0500

    f

--- cataclysm_website.client/vite.config.ts
+++ cataclysm_website.client/vite.config.ts
@@ -25,7 +25,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
         'Pem',
         '--no-password',
     ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
+        //throw new Error("Could not create certificate.");
     }
 }
 
