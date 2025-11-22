1cf803fcd8632df71b42c73113eba2defac0b4d0
Kyle aka Jax <kskeller@live.com>
2025-11-22 15:49:14 -0500

    Add new server names to UsRealms and EuRealms

--- cataclysm_website.client/src/clients/ServerNames.ts
+++ cataclysm_website.client/src/clients/ServerNames.ts
@@ -1,10 +1,14 @@
 const UsRealms = ["Atiesh","Myzrael","Old Blanchy","Azuresong","Mankrik","Pagle",
     "Ashkandi","Westfall","Whitemane","Faerlina","Grobbulus",
-    "Bloodsail Buccaneers","Remulos","Arugal","Yojamba","Skyfury",
+    "Bloodsail Buccaneers","Remulos-au","Arugal-au","Yojamba-au","Skyfury",
     "Sulfuras","Windseeker","Benediction",
-    "Earthfury","Maladath","Angerforge","Eranikus"];
+    "Earthfury","Maladath","Angerforge","Eranikus", "Galakras", "Immerseus",
+    "Lei Shen", "Nazgrim", "Raden"];
 const EuRealms = ["Everlook","Auberdine","Lakeshire","Chromie",
     "Pyrewood Village","Mirage Raceway","Razorfen", "Firemaw",
-    "Nethergarde Keep","Sulfuron","Golemagg"];
+    "Nethergarde Keep","Sulfuron","Golemagg","Amnennar","Ashbringer","Earthshaker",
+    "Flamegor","Garalon","Gehennas","Giantstalker","Hoptallus","Hydraxian Waterlords",
+    "Jindo","Mograine","Norushen","Ook Ook","Patchwerk","Shekzeer","Thekal","Transcendence",
+    "Venoxis"];
 
 export {UsRealms, EuRealms};
